514f410064ff90e24f13f6e8735883078de70546
sun <xu>
2023-04-10 16:44:15 +0800

    更改了mock

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
+ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteMockServe } from 'vite-plugin-mock'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+ plugins: [vue(),viteMockServe()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -20,4 +22,5 @@ export default defineConfig({
       },
     }
   }
-})
+}) 
+
