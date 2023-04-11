f5c2dca1886670de5268388712b82d27b9eed1bb
sun <xu>
2023-04-11 09:11:14 +0800

    更新了首页

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 4e410922b986a83051865241fd582d7625b883b4
 import { createStore } from 'vuex'
 // import users from './modules/users'
 import createPersistedState from "vuex-persistedstate";
