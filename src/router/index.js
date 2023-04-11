cb5d9feec9a79c5de66d3b1aa82daf75fb9c3f2d
d <new666@qq.com>
2023-04-11 10:45:25 +0800

    添加了messsage.vue,在index添加路由message

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const Login = () => import('../views/Login.vue')
 const Index = () => import('../views/Index.vue')
+const message = () => import('../views/message.vue')
 
 
 
@@ -16,6 +17,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/message',
+      name: 'message',
+      component: message
     }
   ]
 })
