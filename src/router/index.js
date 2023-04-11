be9c3c5a2a8ff7c3d4b6e9d48c520a1fc1d07ad7
haha <haha123@.com>
2023-04-11 18:40:20 +0800

    贷款页面更新

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const Login = () => import('../views/Login.vue')
 const Index = () => import('../views/Index.vue')
+const UserLoan = () => import('../views/userloan/Index.vue')
+const LoanApplicationForm = () => import('../views/userloan/LoanApplicationForm.vue')
+const SubmitSuccess = () => import('../views/userloan/SubmitSuccess.vue')
+const ApplicationDetail = () => import('../views/userloan/ApplicationDetail.vue')
 
-
-
+ApplicationDetail
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,6 +19,26 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    ,{
+      path: '/userloan',
+      name: 'userLoanindex',
+      component: UserLoan
+    },
+    {
+      path: '/loanform',
+      name: 'loanApplicationForm',
+      component: LoanApplicationForm
+    },
+    {
+      path: '/submitsuccess',
+      name: 'submitSuccess',
+      component: SubmitSuccess
+    },
+    {
+      path: '/applicationdetail',
+      name: 'applicationDetail',
+      component: ApplicationDetail
     }
   ]
 })
